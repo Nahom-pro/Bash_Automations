8796d2a1cc383836c273fead544680acbc6ad0ca
Nahom-pro <nahomesubalew2000@gmail.com>
2025-03-15 13:59:37 +0300

    Update iwconfig.sh

--- interface_mode/iwconfig.sh
+++ interface_mode/iwconfig.sh
@@ -1,4 +1,9 @@
+# Bring the interface down
 sudo ifconfig wlan0 down
+
+# Set the interface to monitor mode
 sudo sudo iwconfig wlan0 mode monitor
 #change wlan0 to your interface name
+
+# Bring the interface up
 sudo ifconfig wlan0 up
