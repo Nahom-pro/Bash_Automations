29f7fbe83445121d5997ef8aeb2f5216b26fb0ae
Nahom-pro <nahomesubalew2000@gmail.com>
2025-03-15 14:03:06 +0300

    Update iw.sh

--- interface_mode/iw.sh
+++ interface_mode/iw.sh
@@ -1,3 +1,8 @@
+# Bring the interface down
 sudo ip link set wlan0 down
+
+# Set the interface to monitor mode
 sudo iw dev wlan0 set type monitor
+
+# Bring the interface up
 sudo ip link set wlan0 up
